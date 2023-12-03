7406e4a007bdead6141310e8a56e8aa6d175f0d9
chanhee060710 <ppp940304ppp@gmail.com>
2023-12-03 21:57:45 +0900

    test

---
+++ app/src/main/java/com/example/a2023webapp/FindPage.java
@@ -0,0 +1,14 @@
+package com.example.a2023webapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_page);
+    }
+}
